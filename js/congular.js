33e4ad96314583fb974cbcd3f675bb39142cc918
edward12699 <edward12699@163.com>
2015-11-04 18:24:44 +0800

    1

---
+++ js/congular.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xiaoxiang on 2015/11/3.
+ */
+$(document).ready(function(){
+		$(".button").click(function(){
+				$(".commit_info").fadeIn();
+
+		})
+})
---
+++ js/help.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xiaoxiang on 2015/11/3.
+ */
+$(document).ready(function(){
+		$(".wenzi").click(function(){
+				$(".button:eq(0)").hide();
+				$(".back span").html("拆开啦！太感谢你了！<br/>你也去领一个吧！")
+		})
+})
